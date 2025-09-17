25d14bff8880e3ff7e9d6d6be284a9983a01b26a
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-09-17 07:24:14 +0100

    created orderDetail page

--- types/products.ts
+++ types/products.ts
@@ -16,3 +16,18 @@ export type FoodItemProps = {
   image_url: string;
   description: string;
 };
+
+
+
+interface OrderItem {
+  id: string;
+  product: Product;
+  quantity: number;
+}
+
+interface Order {
+  id: string;
+  order_status: "pending" | "processing" | "completed" | "cancelled";
+  created_at: string;
+  order_items: OrderItem[];
+}
